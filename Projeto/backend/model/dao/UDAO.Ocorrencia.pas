8342a641a1c8b0af836ed42f15887348c94e0276
Max Furtado <maximilianomfurtado@gmail.com>
2023-03-15 00:22:13 -0300

    implement update ocorrencia

--- Projeto/backend/model/dao/UDAO.Ocorrencia.pas
+++ Projeto/backend/model/dao/UDAO.Ocorrencia.pas
@@ -3,7 +3,7 @@ unit UDAO.Ocorrencia;
 interface
 
 uses
-  UDAO.Base, System.JSON;
+  UDAO.Base, System.JSON, UEntity.Ocorrencias;
 
 type
   TDAOOcorrencia = class(TDAOBase)
@@ -19,15 +19,93 @@ type
       function ObterRegistrosPorBairro(aBairro: String): TJSONArray;
       function ObterRegistrosPorLogradouro(aLogradouro: String): TJSONArray;
       function ProcurarPorId(const aIdentificador: Integer): TJSONObject; override;
+      function ProcurarPorIdComIndex(const aIdentificador: Integer): TJSONObject;
+      function AtualizarRegistro(const aId: Integer;
+        const aStatus, aQtdApoio: Integer): Boolean;
   end;
 
 implementation
 
 uses
   System.SysUtils, UDAO.Intf, UDAO.Status, UDAO.TipoProblema, UDAO.Usuario,
-  UDAO.Endereco, UUtil.Banco;
+  UDAO.Endereco, UUtil.Banco, DateUtils;
 
 { TDAOOcorrencia }
+
+function TDAOOcorrencia.AtualizarRegistro(const aId: Integer;
+        const aStatus, aQtdApoio: Integer): Boolean;
+var
+  xJSONArray: TJSONArray;
+  xJSON: TJSONObject;
+  xOcorrencia: TOcorrencia;
+  xDataInicial, xDataFinal, xDataAlteracao: String;
+  xQuery, xStringData: String;
+  xData: TDateTime;
+begin
+  Result := False;
+
+  xJSON := TJSONObject.Create;
+  xJSONArray := TJSONArray.Create;
+  xJSON := Self.ProcurarPorIdComIndex(aId);
+
+  xStringData := xJSON.GetValue<String>('datafinal');
+  if xStringData = EmptyStr then
+    xDataFinal := 'null'
+  else
+  begin
+  xData := ISO8601ToDate(xJSON.GetValue<String>('datafinal'));
+  xDataFinal := FormatDateTime('yyyy-mm-dd hh:nn:ss', xData);
+  end;
+
+
+  xStringData := xJSON.GetValue<String>('datainicial');
+  if xStringData = EmptyStr then
+    xDataInicial := 'null'
+  else
+  begin
+    xData := ISO8601ToDate(xJSON.GetValue<String>('datainicial'));
+    xDataInicial := FormatDateTime('yyyy-mm-dd hh:nn:ss', xData);
+  end;
+
+  xStringData := xJSON.GetValue<String>('dataalteracao');
+  if xStringData = EmptyStr then
+    xDataAlteracao := 'null'
+  else
+  begin
+    xData := ISO8601ToDate(xJSON.GetValue<String>('dataalteracao'));
+    xDataAlteracao := FormatDateTime('yyyy-mm-dd hh:nn:ss', xData);
+  end;
+
+  xQuery := 'UPDATE OCORRENCIA SET id = %d, urgencia = %d, '
+   + 'descricao = "%s", qntapoio = %d, '
+   + 'idusuario = %d, idtipoproblema = %d, idstatus = %d, idendereco = %d ,';
+   xQuery := Format(xQuery,[
+    aId,
+    xJSON.GetValue<Integer>('urgencia'),
+    xJSON.GetValue<String>('descricao'),
+    aQtdApoio,
+    xJSON.GetValue<Integer>('idusuario'),
+    xJSON.GetValue<Integer>('idtipoproblema'),
+    aStatus,
+    xJSON.GetValue<Integer>('idendereco')]);
+
+   if not (xDataFinal = 'null') then
+    xQuery := xQuery +  'datafinal = "' + xDataFinal + '" ';
+   if not (xDataInicial = 'null') then
+    xQuery := xQuery +  'datainicial = "' + xDataInicial + '" ';
+   if not (xDataAlteracao = 'null') then
+    xQuery := xQuery +  'dataalteracao = "' + xDataAlteracao + '" ';
+
+   xQuery := xQuery +  'WHERE id = ' + aId.ToString;
+
+  try
+    Result := TUtilBanco.UpdateRegistro(xQuery);
+  except
+    on e: Exception do
+      raise Exception.Create('Erro ao Obter Registros: ' + e.Message);
+  end;
+  FreeAndNil(xJSONArray);
+end;
 
 constructor TDAOOcorrencia.Create;
 begin
@@ -276,6 +354,12 @@ begin
   xJSONObject.RemovePair('idtipoproblema');
 
   Result := xJSONObject;
+end;
+
+function TDAOOcorrencia.ProcurarPorIdComIndex(
+  const aIdentificador: Integer): TJSONObject;
+begin
+  Result:= inherited ProcurarPorId(aIdentificador)
 end;
 
 function TDAOOcorrencia.ProcurarStatus(const aId: Integer): String;
